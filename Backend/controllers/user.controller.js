c1135f7dcf827446024b44a73295cf7eba39a149
Ravish Singh <singhraviit807@gmail.com>
2025-10-11 20:16:38 +0530

    register captain done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -11,11 +11,16 @@ module.exports.registerUser = async (req, res, next) => {
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-
-    console.log(req.body);
     
 
     const { fullname, email, password } = req.body;
+
+    const isUserAlready = await userModel.findOne({ email });
+
+    if (isUserAlready) {
+        return res.status(400).json({ message: 'User already exist' });
+    }
+
 
     const hashedPassword = await userModel.hashPassword(password);
 
